c0baf2c49eb3c510200854b67269a1b102090613
asolda <a.solda92@gmail.com>
2017-10-16 15:23:05 +0100

    Data regularization and new models

--- tennis_clf.py
+++ tennis_clf.py
@@ -1,18 +1,22 @@
 import numpy as np
 import pandas as pd
 
+from sklearn import svm, tree
 from sklearn import preprocessing, utils
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report
 from sklearn.model_selection import cross_val_predict
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 
 data = pd.read_csv('data/match_matrix.csv', delimiter=', ', engine='python').dropna(axis=0)
 
+'''
 le = preprocessing.LabelEncoder()
 to_encode = ['Court', 'Surface', 'Round', 'Series']
 for col in to_encode:
     le.fit(data[col])
     data[col] = le.transform(data[col])
+'''
 
 def invert(df):
     to_return = df.copy()
@@ -39,19 +43,31 @@ second = data.tail((len(data)-len(first)))
 second = invert(second)
 data = pd.concat([first, second])
 data.drop(['Winner', 'Loser', 'Wsets', 'Lsets'], inplace=True, axis=1)
-data = utils.shuffle(data.values, random_state=42)
 
-X = np.array([elem[:-2] for elem in data])
-y = np.array([elem[-2:] for elem in data])
+data = pd.concat([
+    data,
+    pd.get_dummies(data['Surface']),
+    pd.get_dummies(data['Court']),
+    pd.get_dummies(data['Round']),
+    pd.get_dummies(data['Series'])
+], axis=1)
+
+data.drop(['Surface', 'Court', 'Round', 'Series'], inplace=True, axis=1)
+
+data = utils.shuffle(data, random_state=42)
+
+y = np.array(data['LDigit'])
+X = data.drop(['WDigit', 'LDigit'], axis=1).values
+
+X = preprocessing.scale(X)
 
 print('Input shape:', X.shape)
 
 models = {
-    '(128, 128)': MLPClassifier(
-        solver='lbfgs',
-        alpha=1e-5,
-        hidden_layer_sizes=(128, 128),
-        random_state = 1),
+    'Support Vector Machine (Linear)': svm.SVC(kernel='linear'),
+    'AdaBoost': AdaBoostClassifier(),
+    'Decision Tree': tree.DecisionTreeClassifier(),
+    'Random Forest': RandomForestClassifier(),
     '(16, 16, 16, 32, 16, 16)': MLPClassifier(
         solver='lbfgs',
         activation="relu",
@@ -64,23 +80,11 @@ models = {
         alpha=1e-5,
         hidden_layer_sizes=(16, 16, 32, 64, 32, 16),
         random_state = 1),
-    '(32, 32, 64, 128, 64, 32)': MLPClassifier(
-        solver='lbfgs',
-        activation="relu",
-        alpha=1e-5,
-        hidden_layer_sizes=(32, 32, 64, 128, 64, 32),
-        random_state = 1),
     '(16, 16, 32, 64, 64, 32, 16)': MLPClassifier(
         solver='lbfgs',
         activation="relu",
         alpha=1e-5,
         hidden_layer_sizes=(16, 16, 32, 64, 64, 32, 16),
-        random_state = 1),
-    '(16, 32, 64, 64, 128, 32, 16)': MLPClassifier(
-        solver='lbfgs',
-        activation="relu",
-        alpha=1e-5,
-        hidden_layer_sizes=(16, 32, 64, 64, 128, 32, 16),
         random_state = 1)
 }
 
